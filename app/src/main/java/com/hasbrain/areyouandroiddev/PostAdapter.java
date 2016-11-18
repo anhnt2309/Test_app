a8d28797893245716157a15ffb05a03f1c3fab4c
Tuan Anh <anhnt.2309@gmail.com>
2016-11-18 13:52:13 +0700

    Initial commit

---
+++ app/src/main/java/com/hasbrain/areyouandroiddev/PostAdapter.java
@@ -0,0 +1,49 @@
+package com.hasbrain.areyouandroiddev;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.hasbrain.areyouandroiddev.model.RedditPost;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostAdapter extends ArrayAdapter<RedditPost> {
+
+    public PostAdapter(Activity context, List<RedditPost> redditPosts){
+        super(context,0,redditPosts);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView =convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.activity_post_list,parent, false);
+        }
+        RedditPost currentRedditPost = getItem(position);
+        TextView score =(TextView) listItemView.findViewById(R.id.post_score);
+        score.setText(""+currentRedditPost.getScore());
+        TextView authors = (TextView) listItemView.findViewById(R.id.author);
+        authors.setText(""+currentRedditPost.getAuthor());
+        TextView domain = (TextView) listItemView.findViewById(R.id.domain);
+        domain.setText(""+currentRedditPost.getDomain());
+        TextView title = (TextView) listItemView.findViewById(R.id.post_title);
+        title.setText(""+currentRedditPost.getTitle());
+        TextView comment = (TextView) listItemView.findViewById(R.id.no_comment);
+        comment.setText(""+currentRedditPost.getCommentCount());
+
+
+        return listItemView;
+    }
+}
---
+++ app/src/main/java/com/hasbrain/areyouandroiddev/PostInSectionActivity.java
@@ -0,0 +1,24 @@
+package com.hasbrain.areyouandroiddev;
+
+import android.widget.ListView;
+
+import com.hasbrain.areyouandroiddev.model.RedditPost;
+
+import java.util.List;
+
+/**
+ * Created by Jupiter (vu.cao.duy@gmail.com) on 10/9/15.
+ */
+public class PostInSectionActivity extends PostListActivity {
+
+    @Override
+    protected void displayPostList(List<RedditPost> postList) {
+        //TODO: Display posts in sections.
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_post_in_section;
+    }
+}
